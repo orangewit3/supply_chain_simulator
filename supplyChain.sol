66726121e06fd802d361ed991eb463a479b95c48
orangewit3 <43012690+orangewit3@users.noreply.github.com>
2020-05-31 15:40:01 -0700

    included global transactions for farmers and manufacturers

--- supplyChain.sol
+++ supplyChain.sol
@@ -2,35 +2,48 @@ pragma solidity ^0.4.20;
 
 
 contract cocoBeanFarmer {
-    
+
     int quantity = 0; // total beans this farmer has 
-    
-    constructor(int initial_quantity) public {
+    uint256[] pendingTransactions;  
+    SupplyChainTransactions private _globalTransactions;
+
+    constructor(int initial_quantity, address transactionAddress) public {
         quantity = initial_quantity;
+        _globalTransactions = SupplyChainTransactions(transactionAddress);
+        _globalTransactions.addTransactionCreator(this);
+        _globalTransactions.addSupplyChainParty(this, 0);
     }
-    
-    function balance() external view returns (int) {
+
+    function beanBalance() external view returns (int) {
         return quantity;
     }
-    
+
     function claimBeans(int num_beans) external {
         quantity = quantity + num_beans;
     }
     
-    function sendBeans(address _to, int num_beans) external {
-        require(quantity > num_beans);
-        quantity = quantity - num_beans;
+    function createBeanTransaction(string name, string description, uint256 quantity) {
+        uint256 transactionID = _globalTransactions.addTransaction(name, description, quantity);
+        pendingTransactions.push(transactionID);
+    }
+    
+    function sendBeanTransaction(uint256 transactionID, address recipient) {
+        _globalTransactions.addRecipient(transactionID, recipient);
     }
 }
 
 contract manufacturer {
     int bean_count = 0; // num_beans this manufacturer has 
+    uint256[] pendingTransactions; 
+    SupplyChainTransactions private _globalTransactions;
     
     int beansToCoffeeRatio = 50; // need 50 beans to produce 1 coffee 
     
-    constructor(int initial_bean_count, int estimated_beans_to_coffee_ratio) {
+    constructor(int initial_bean_count, int estimated_beans_to_coffee_ratio, address transactionAddress) {
         bean_count = initial_bean_count;
         beansToCoffeeRatio = estimated_beans_to_coffee_ratio;
+        _globalTransactions = SupplyChainTransactions(transactionAddress);
+        _globalTransactions.addSupplyChainParty(this, 1);
     }
     
     function capacity() internal returns (int) {
@@ -50,12 +63,10 @@ contract manufacturer {
         require(cap > quantity);
         bean_count = bean_count - (quantity * beansToCoffeeRatio);
     }
-    
-    
 }
 
 contract SupplyChainTransactions {
-    
+
     struct Status {
         bool accepted; // the requesting party is in compliance with transaction attributes
         bool verified; // the verifiable party has verified the product
@@ -68,13 +79,13 @@ contract SupplyChainTransactions {
         uint256 quantity;
         Status status;
         address currentOwner;
+        address recipient;
     }
-    
+
     mapping (uint256 => address) public transactionToOwner; 
     mapping (address => bool) private authorizedTransactionCreators;
     mapping (address => int) private partyToStage;
-    
-    
+
     Transaction[] transactions;
 
     function addTransaction(string name, string description, uint256 quantity) external returns (uint256 transactionID) {
@@ -83,33 +94,59 @@ contract SupplyChainTransactions {
             verified: false,
             premature: true
         });
-        
+
         require(authorizedTransactionCreators[msg.sender]);
-        
+
         Transaction memory transaction = Transaction({
             name: name,
             description: description,
             quantity: quantity,
             status: status,
-            currentOwner: msg.sender
+            currentOwner: msg.sender,
+            recipient: msg.sender
         });
-        
+
         transactionID = transactions.push(transaction) - 1;
     }
-    
+
     function verifyTransaction(uint256 transactionID) {
         int verifierStage = partyToStage[msg.sender];
         Transaction transaction = transactions[transactionID];
         require(verifierStage == partyToStage[transaction.currentOwner] - 1);
         transaction.status.verified = true;
     }
-    
+
+    function addRecipient(uint256 transactionID, address recipient) {
+        transactions[transactionID].recipient = recipient;
+    }
+
+    function transferTransactionOwnership(uint256 transactionID) {
+        transactions[transactionID].currentOwner = msg.sender;
+        transactionToOwner[transactionID] = msg.sender;
+    }
+
+    function getTransactionOwner(uint256 transactionID) returns (address) {
+        return transactionToOwner[transactionID];
+    }
+
     function addTransactionCreator(address _address) public view {
         authorizedTransactionCreators[_address] = true;
     }
-    
+
     function addSupplyChainParty(address _address, int stage) public view {
         partyToStage[_address] = stage;
     }
+
+    function isTransactionVerified(uint256 transactionID) returns (bool) {
+        return transactions[transactionID].status.verified;
+    }
+
+    function isTransactionAccepted(uint256 transactionID) returns (bool) {
+        return transactions[transactionID].status.accepted;
+    }
     
+    function getTransactionName(uint256 transactionID) returns (string) {
+        return transactions[transactionID].name;
+    }
+
 }
