070f4ac41ccee0d6af4e431a1c182c52afe53153
Nakul Iyer <nakulpiyer@gmail.com>
2020-11-04 18:32:45 -0500

    added prelim tests for farmer

--- supplychain-sim-interface/test/supplyChain.test.js
+++ supplychain-sim-interface/test/supplyChain.test.js
@@ -1,9 +1,9 @@
 /**
- * @dev 
+ * @dev
  * One test file for all contracts in `supplyChain.test.js`
  */
 const assert = require('assert')
-const ganache = require("ganache-core")
+const ganache = require('ganache-core')
 const Web3 = require('web3')
 // Making an instance of Web3
 const web3 = new Web3(ganache.provider())
@@ -20,16 +20,42 @@ let accounts,
   supplyChainAddress,
   SupplyChainTransactions
 
-
-/**
- * @todo 
- * Write tests for the `cocoaBeanFarmer.json` ABI
- */
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts()
-  factory = await new web3.eth.Contract(JSON.parse(compiledCocoaBeanFarmer.interface))
-    .deploy({ data: compiledCocoaBeanFarmer.bytecode })
-    .send({ from: accounts[ 0 ], gas: '1000000' })
+  SupplyChainTransactions =
+    await new web3.eth.Contract(compiledSupplyChainTransactions.abi)
+      .deploy({ data: compiledSupplyChainTransactions.bytecode })
+      .send({ from: accounts[ 0 ], gas: 1500000, gasPrice: '300000000' })
 
-  await factory.methods.beanBalance()
+  cocoBeanFarmer = await new web3.eth.Contract(compiledCocoaBeanFarmer.abi)
+    .deploy({
+      data: compiledCocoaBeanFarmer.bytecode,
+      arguments: [100, SupplyChainTransactions.options.address]
+    })
+    .send({ from: accounts[ 0 ], gas: 1500000, gasPrice: '30000000000' })
+})
+
+describe('Cocoa Bean Farmer', () => {
+  it('can get beans', async () => {
+    assert.equal(100, (await cocoBeanFarmer.methods.beanBalance().call()))
+  })
+
+  it('can claim beans', async () => {
+    await cocoBeanFarmer.methods.claimBeans(15).send({ from: accounts[ 0 ] })
+    assert.equal(115, (await cocoBeanFarmer.methods.beanBalance().call()))
+  })
+
+  it('can get current balance', async () => {
+    assert.equal(0, (await cocoBeanFarmer.methods.etherBalance().call()))
+  })
+
+  it('can accept ether', async () => {
+    let receipt = await web3.eth.sendTransaction({
+      from: accounts[ 1 ],
+      to: cocoBeanFarmer.options.address,
+      value: web3.utils.toWei('1', 'ether')
+    })
+    assert(receipt.status)
+    assert.equal(web3.utils.toWei('1', 'ether'), (await cocoBeanFarmer.methods.etherBalance().call()))
+  })
 })
