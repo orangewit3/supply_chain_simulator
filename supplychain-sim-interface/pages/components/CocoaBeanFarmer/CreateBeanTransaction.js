841e4035e6997804cc994d84ba473aad444322bd
Jack Winfield <platocrat@tuta.io>
2020-12-11 23:10:22 -0600

    left off on create bean transaction

--- supplychain-sim-interface/pages/components/CocoaBeanFarmer/CreateBeanTransaction.js
+++ supplychain-sim-interface/pages/components/CocoaBeanFarmer/CreateBeanTransaction.js
@@ -1,30 +1,12 @@
 import React, { useState } from 'react'
 import contract from '../../../lib/ethereum/contract-api/cocoaBeanFarmer'
 
-
-let CocoaBeanFarmer = contract
-
 /**
- * @dev Current attempt at decoupling boolean. See next dev comment for more 
- * info.
+ * @dev 
+ * @todo
+ * Finish this component and render event log data on client table
  */
-export const showBeanEvent = false
 
-/**
- * @dev Below is Jason's previous attempt at decoupling boolean of 
- * `showBeanEvent`, a boolean that we can use in our higher-order component, 
- * `index.js`, to perform an if-check to display the event data of the newly 
- * created transaction.
- */
-// export async function useBeanEvent(showBeanEvent) {
-//   if (!showBeanEvent) {
-//     const [ showBeanEvent, setBeanEvent ] = useState(false)
-
-//     return [ showBeanEvent, setBeanEvent ]
-//   }
-
-//   return showBeanEvent
-// }
 
 const CreateBeanTransactionForm = () => {
   // State for form
@@ -32,47 +14,6 @@ const CreateBeanTransactionForm = () => {
   const [ beanTxnDescription, setBeanTxnDescription ] = useState()
   const [ beanTxnQuantityToSend, setBeanTxnQuantityToSend ] = useState()
 
-
-  const handleCreateBeanTransaction = async () => {
-    const contract = new web3.eth.Contract(
-      CocoaBeanFarmer.abi,
-      // Harded code for demo
-      "0xcaDbE04877927d4450335FDbB4b2EA2018883cD2",
-    )
-
-    /**
-     * @dev If you uncomment the next 3 lines, then this allows MetaMask to
-     * request to be connected to the app after we make a call to the
-     * `createBeanTransaction` function.
-     */
-    const ethereum = window.ethereum
-    const web3Instance = new Web3(ethereum)
-    const enabledWeb3 = await ethereum.enable()
-    const account = await web3.eth.getAccounts()
-    const accountAddress = await account[ 0 ]
-
-    /**
-     * @dev
-     * @todo We need an if-check to validate the form has all fields filled
-     */
-    // if (
-    //   beanTxnName != 'txn1' &&
-    //   beanTxnDescription != 'first bean txn' &&
-    //   beanTxnQuantityToSend != '10'
-    // ) {
-    await contract.methods
-      .createBeanTransaction(
-        beanTxnName,
-        beanTxnDescription,
-        beanTxnQuantityToSend.toString()
-      ).send({ from: accountAddress })
-
-    showBeanEvent = true
-
-    //   } else {
-    //     console.log()
-    //   }
-  }
 
   return (
     <div class="grid max-h-screen place-items-center">
